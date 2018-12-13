ef68fe4af77d3078a2fe7fc624862b795828657b
AmanullahAshraf <45756373+AmanullahAshraf@users.noreply.github.com>
2018-12-13 18:02:26 +0530

    Updated test_vec.cpp
    Updated test_vec.cpp as per new behavior of connector ash_STL.h

--- test_vec.cpp
+++ test_vec.cpp
@@ -1,53 +1,80 @@
 #include <iostream>
 #include <string.h>
-#include "ash_STL.h"
+#include "test_STL.h"
+
+#define OBJECT_LIMITATION 50;
+
 using namespace std;
 
 class Target
 {
-	int var;
-	public:
-	Target()
-	{
-	}
-	Target(int x)
-	{
-		this->var = x;
-	}
-	int get_var()
-	{
-		return var;
-	}
+        static int visible;
+        int var;
+        public:
+        Target()
+        {
+                visible++;
+                var = visible;
+        }
+        Target(int x)
+        {
+                this->var = x;
+        }
+        int get_var()
+        {
+                return var;
+        }
 };
 
-void print_vector(ash_vec::vector<int> &v)
-{
-	cout << "Start printing..." << endl;
-	Connector<int> c_st = v.begin();
-	Connector<int> c_end = v.end();
-	while(*c_st != *c_end)
-	{
-		cout << "Data from vector : " << *(*c_st) << endl;
-		c_st++;
-	}
-		cout << "Data from vector : " << *(*c_st) << endl;
-}
+int Target::visible = 0;
 
+void print_vector(ash_vec::vector<Target> &v)
+{
+        cout << "Start printing..." << endl;
+        Connector<Target> c_st = v.begin();
+        Connector<Target> c_end = v.end();
+
+        while(*c_st != *c_end)
+        {
+                cout << "Data from vector : " << (*c_end)->get_var() << endl;
+                c_end--;
+        }
+                cout << "Data from vector : " << (*c_end)->get_var() << endl;
+}
 int main()
 {
-	ash_vec::vector<int>v;
-	for(int i = 0 ; i < 20 ; i++)
-	{
-		v.push_back(i);
-	}	
+#if 0
+        ash_vec::vector<int>v;
+        /*Pushing data in*/
+        for(int i = 0 ; i < 20 ; i++)
+        {
+                v.push_back(i);
+        }
+        print_vector(v);
 
-	print_vector(v);
+        /*Poping data out*/
+        for(int i = 0 ; i < 5 ; i++)
+        {
+                v.pop_back();
+        }
+        print_vector(v);
+#endif
+        int obj_count = OBJECT_LIMITATION;
+        Target *tar = new Target[obj_count];
+        ash_vec::vector<Target> v;
+        int index = 0;
+	
+	while(index != obj_count)
+        {
+                v.push_back(tar[index++]);
+        }
+        print_vector(v);
+        /*Poping data out*/
+        for(int i = 0 ; i < 10 ; i++)
+        {
+                v.pop_back();
+        }
+        print_vector(v);
 
-	for(int i = 0 ; i < 10 ; i++)
-	{
-		v.pop_back();
-	}
-
-	print_vector(v);
-	return 0;
+        return 0;
 }
